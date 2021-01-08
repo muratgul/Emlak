19b8e604d5846b36e4860bbdeeede371d6c629df
Murat Gül <muratgul@gmail.com>
2021-01-08 13:47:52 +0300

    Modified

--- app/src/main/java/com/muratgul/emlak/view/MainActivity.kt
+++ app/src/main/java/com/muratgul/emlak/view/MainActivity.kt
@@ -2,11 +2,13 @@ package com.muratgul.emlak.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.muratgul.emlak.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
